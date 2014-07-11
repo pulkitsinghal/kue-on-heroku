47f5fc13cfca7f0aedfa6abebea435e0658b65f9
Pulkit Singhal <pulkit@shoppinpal.com>
2014-07-11 11:56:54 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,55 @@
+var express = require('express')
+  , kue = require('kue')
+  , url = require('url')
+  , redis = require('kue/node_modules/redis');
+ 
+kue.redis.createClient = function() {
+    var redisUrl = url.parse(process.env.REDISCLOUD_URL)
+      , client = redis.createClient(redisUrl.port, redisUrl.hostname);
+    if (redisUrl.auth) {
+        client.auth(redisUrl.auth.split(":")[1]);
+    }
+    return client;
+};
+ 
+// then access the current Queue
+var jobs = kue.createQueue()
+  , app = express.createServer();
+ 
+app.get('/', function(req, res) {
+    
+    var job = jobs.create('crawl', {
+        url: 'http://example.com'
+      , token: 'foo'
+    });
+ 
+    job.on('complete', function(){
+        // avoid sending data after the response has been closed
+        if (res.finished) {
+            console.log("Job complete");
+        } else {
+            res.send("Job complete");
+        }
+    }).on('failed', function(){
+        if (res.finished) {
+            console.log("Job failed");
+        } else {
+            res.send("Job failed");
+        }
+    }).on('progress', function(progress){
+        console.log('job #' + job.id + ' ' + progress + '% complete');
+    });
+    
+    job.save();
+ 
+    // timeout after 5s
+    setTimeout(function() {
+        res.send("OK (timed out)");
+    }, 5000);
+});
+ 
+// wire up Kue (see /active for queue interface)
+app.use(kue.app);
+ 
+app.listen(process.env.PORT);
+console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
---
+++ consumer.js
@@ -0,0 +1,23 @@
+var kue = require('kue')
+  , url = require('url')
+  , redis = require('kue/node_modules/redis');
+ 
+// make sure we use the Heroku Redis To Go URL
+// (put REDISTOGO_URL=redis://localhost:6379 in .env for local testing)
+ 
+kue.redis.createClient = function() {
+    var redisUrl = url.parse(process.env.REDISTOGO_URL)
+      , client = redis.createClient(redisUrl.port, redisUrl.hostname);
+    if (redisUrl.auth) {
+        client.auth(redisUrl.auth.split(":")[1]);
+    }
+    return client;
+};
+ 
+var jobs = kue.createQueue();
+ 
+// see https://github.com/learnBoost/kue/ for how to do more than one job at a time
+jobs.process('crawl', function(job, done) {
+  console.log(job.data);
+  done();
+});
